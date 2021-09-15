e42dcb1b7feaaa3ed3db9c48b4d1b81017a1fb7e
Nils <herrnielsson.nd@gmail.com>
2021-09-15 22:12:00 +0100

    Added Client and Core sided logging

--- Gear5th/src/Gear5th.h
+++ Gear5th/src/Gear5th.h
@@ -4,6 +4,7 @@
 
 #include "Gear5th/Application.h"
 #include <stdio.h>
+#include "Gear5th/Log.h"
 
 // -------- ENTRY POINT ----------------
 #include "Gear5th/EntryPoint.h"
--- Gear5th/src/Gear5th/EntryPoint.h
+++ Gear5th/src/Gear5th/EntryPoint.h
@@ -6,8 +6,15 @@ extern Gear5th::Application* Gear5th::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Gear5th.dll linked properly, the app is using the builtin EntryPoint provided by Gear5th.dll!");
+	//printf("Gear5th.dll linked properly, the app is using the builtin EntryPoint provided by Gear5th.dll!");
 	
+	Gear5th::Log::Init();
+	G5_CORE_WARN("Initialized Engine Logger");
+
+	G5_INFO("Initialized Client-side Logger");
+	G5_CORE_ERROR("caught up in while(true)");
+	
+
 	auto app = Gear5th::CreateApplication();
 	app->Run();
 
